b49631a13e4f8c2f20d5c3d204e3a8baf8bc14c8
daichi0918 <kayasugadaichi@icloud.com>
2024-12-09 16:47:26 +0900

    getStateによるstateの状態管理を確認

--- redux-learn/src/components/count.jsx
+++ redux-learn/src/components/count.jsx
@@ -1,16 +1,18 @@
-import { connect } from 'react-redux';
+import { useSelector } from 'react-redux';
 
-function Count({ count }) {
+function Count() {
+  const count = useSelector((state) => state.count);
+  const posts = useSelector((state) => state.posts);
   return (
     <>
       <div>Countコンポーネント:{count}</div>
+      <ul>
+        {posts.map((post) => (
+          <li key={post.id}>{post.title}</li>
+        ))}
+      </ul>
     </>
   );
 }
-
-const mapStateToProps = (state) => {
-  return { count: state.count };
-};
-
-export default connect(mapStateToProps)(Count);
+export default Count;
 // export default Count;
