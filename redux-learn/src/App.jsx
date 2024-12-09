d5fa77d857de99a33e2a08bda00c3d7328ffa2c1
daichi0918 <kayasugadaichi@icloud.com>
2024-12-10 08:52:52 +0900

    complete:redux-learn

--- redux-learn/src/App.jsx
+++ redux-learn/src/App.jsx
@@ -1,16 +1,24 @@
 import { useState } from 'react';
 import './App.css';
 import store from './store/index';
-import { connect, useSelector } from 'react-redux';
+import { connect, useDispatch, useSelector } from 'react-redux';
 import Count from './components/count';
 
 function App() {
-  const count = useSelector((state) => state.countReducer.count);
+  const count = useSelector((state) => state.count);
+  const dispatch = useDispatch();
+  const increase = () => {
+    dispatch({ type: 'INCREASE_COUNT' });
+  };
+  const decrease = () => {
+    dispatch({ type: 'DECREASE_COUNT' });
+  };
   return (
     <div>
       <h1>Redux Learn</h1>
       <p>Count: {count}</p>
-      <Count />
+      <button onClick={increase}>UP</button>
+      <button onClick={decrease}>DOWN</button>
     </div>
   );
 }
--- redux-learn/src/store/index.js
+++ redux-learn/src/store/index.js
@@ -1,33 +1,52 @@
 import { combineReducers, createStore } from 'redux';
 
-const countReducer = (
-  state = {
-    count: 50,
-  }
-) => {
-  return state;
+// const countReducer = (
+//   state = {
+//     count: 50,
+//   }
+// ) => {
+//   return state;
+// };
+
+// const postsReducer = (
+//   state = {
+//     posts: [
+//       { id: 1, title: 'Reduxについて' },
+//       {
+//         id: 2,
+//         title: 'ReduxのHooksについて',
+//       },
+//     ],
+//   }
+// ) => {
+//   return state;
+// };
+
+// const rootReducer = combineReducers({
+//   countReducer,
+//   postsReducer,
+// });
+
+const initialStore = {
+  count: 50,
 };
 
-const postsReducer = (
-  state = {
-    posts: [
-      { id: 1, title: 'Reduxについて' },
-      {
-        id: 2,
-        title: 'ReduxのHooksについて',
-      },
-    ],
+const reducer = (state = initialStore, action) => {
+  switch (action.type) {
+    case 'INCREASE_COUNT':
+      return {
+        count: state.count + 1,
+      };
+    case 'DECREASE_COUNT':
+      return {
+        count: state.count - 1,
+      };
+    default:
+      return state;
   }
-) => {
-  return state;
 };
 
-const rootReducer = combineReducers({
-  countReducer,
-  postsReducer,
-});
-
-const store = createStore(rootReducer);
-console.log(store.getState());
+const store = createStore(reducer);
+// console.log(store.getState());
 
 export default store;
